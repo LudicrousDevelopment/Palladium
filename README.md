96059e34d30edef6b74c37d4b25176e38ddf8ac0
EnderKingJ <78042486+EnderKingJ@users.noreply.github.com>
2021-11-02 01:28:58 +0000

    npm package

---
+++ README.md
@@ -0,0 +1,2 @@
+# Palladium
+Web Proxy made by EnderKingJ, intended as a secondary proxy to Womginx or Corrosion, many sites not supported
--- examples/express-server/index.js
+++ examples/express-server/index.js
@@ -1,1 +1,14 @@
 const express = require('express')
+const app = express()
+
+const Smoke = new (require('../../lib/server'))({
+  prefix: '/service/',
+  encode: 'plain',
+  ssl: true,
+})
+
+app.use(express.static(__dirname+'/', {extensions: ['html']}))
+
+app.use(Smoke.express(Smoke))
+
+app.listen(8080)
