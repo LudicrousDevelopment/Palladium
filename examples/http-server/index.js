9d59b9f4e31e0cfc1306f7a0cee331ddea7f14f9
EnderKingJ <78042486+EnderKingJ@users.noreply.github.com>
2021-12-09 03:21:10 +0000

    http server fix

--- examples/http-server/index.js
+++ examples/http-server/index.js
@@ -10,11 +10,12 @@ const Server = http.Server()
 
 const proxy = new Palladium({
   prefix: config.prefix,
-  encode: "plain",
+  encode: "xor",
   ssl: true,
-  debug: true,
+  debug: false,
+  server: Server,
 })
 
-proxy.clientScript().ws(Server)
+proxy.init()
 
 Server.on('request', (req, res) => {if(req.url.startsWith(proxy.prefix)){return proxy.request(req, res)}else{return res.end(fs.readFileSync(__dirname+'/index.html'))}}).listen((process.env['PORT'] || config.port ||  8080), () => {console.log((proxy.ssl ? 'https' : 'http')+'://localhost:'+(process.env['PORT'] || config.port ||  8080))});
