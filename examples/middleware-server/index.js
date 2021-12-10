135dfe7b9a1089ff74d52a6efba1999918740e5f
EnderKingJ <78042486+EnderKingJ@users.noreply.github.com>
2021-12-10 01:03:22 +0000

    fixes

--- examples/middleware-server/index.js
+++ examples/middleware-server/index.js
@@ -11,6 +11,11 @@ const Server = http.Server()
 const proxy = new Palladium({
   prefix: config.prefix,
   encode: "xor",
+  ssl: true,
+  requestMiddleware: [
+    Palladium.blackList(['example.org', 'among.us'], 'Site is Blocked by Host')
+  ],
+  debug: false,
   Corrosion: [false, {}],
   server: Server,
 })
@@ -23,6 +28,6 @@ proxy.onrequest((ctx) => {
   console.log('response', ctx.httpResponse.headers['content-type'] || '')
 })*/
 
-proxy.init();
+proxy.init()
 
 Server.on('request', (req, res) => {if(req.url.startsWith(proxy.prefix)){return proxy.request(req, res)}else{return res.end(fs.readFileSync(__dirname+'/index.html'))}})/*.on('upgrade', (req, socket, head) => proxy.upgrade(req, socket, head))*/.listen((process.env['PORT'] || config.port ||  8080), () => {console.log((proxy.ssl ? 'https' : 'http')+'://localhost:'+(process.env['PORT'] || config.port ||  8080))});
